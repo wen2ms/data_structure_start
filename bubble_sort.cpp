7fcb838fcfa43990f266826c5abdea3e2e4c56ae
wen2ms <1819509111@qq.com>
2025-05-21 18:00:17 +0800

    Feat: bubble sort

---
+++ bubble_sort.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+// O(n^2)
+void bubble_sort(std::vector<int>& vec) {
+    int n = vec.size();
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n - i - 1; ++j) {
+            if (vec[j] > vec[j + 1]) {
+                std::swap(vec[j], vec[j + 1]);
+            }
+        }
+    }
+}
+
+void print_vec(const std::vector<int>& vec) {
+    for (const auto& item : vec) {
+        std::cout << item << ' ';
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    std::vector<int> case_1 = {7, 6, 5, 4, 3, 2};
+    std::vector<int> case_2 = {8, 2, 1, 3, 4};
+    std::vector<int> case_3 = {1, 2, 3};
+
+    bubble_sort(case_1);
+    bubble_sort(case_2);
+    bubble_sort(case_3);
+
+    print_vec(case_1);
+    print_vec(case_2);
+    print_vec(case_3);
+
+    return 0;
+}
