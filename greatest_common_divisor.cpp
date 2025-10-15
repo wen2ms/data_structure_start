d479d0ea5c25122d385c3d827fc2c7adcaf489ee
wen2ms <1819509111@qq.com>
2025-10-15 14:42:50 +0800

    Feat: std::gcd

--- greatest_common_divisor.cpp
+++ greatest_common_divisor.cpp
@@ -1,30 +1,30 @@
 #include <iostream>
-#include <vector>
+#include <numeric>
 
 // int gcd(int a, int b) {
 //     while (b != 0) {
 //         int tmp = a;
-
 //         a = b;
-
 //         b = tmp % b;
 //     }
-
 //     return a;
 // }
+
+// std::gcd
+// std::lcm
 
 int gcd(int a, int b) {
     return b == 0 ? a : gcd(b, a % b);
 }
 
-int lcd(int a, int b) {
+int lcm(int a, int b) {
     return a / gcd(a, b) * b;
 }
 
 int main() {
-    std::cout << gcd(3, 12) << ' ' << lcd(3, 12) << '\n';
+    std::cout << gcd(3, 12) << ' ' << lcm(3, 12) << '\n';
 
-    std::cout << gcd(7, 9) << ' ' << lcd(7, 9) << '\n';
+    std::cout << gcd(7, 9) << ' ' << lcm(7, 9) << '\n';
 
     return 0;
 }
