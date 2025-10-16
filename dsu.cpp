3fd99e9262a95b3f9258aa648df9fd3b1ed7c8c4
wen2ms <1819509111@qq.com>
2025-10-16 13:57:48 +0800

    Feat: DSU

--- dsu.cpp
+++ dsu.cpp
@@ -1,53 +1,50 @@
 #include <iostream>
+#include <numeric>
 #include <vector>
 
-class UnionFind {
+class DSU {
   public:
-    explicit UnionFind(int size) : parents_(size), ranks_(size) {
-        for (int i = 0; i < size; ++i) {
-            parents_[i] = i;
-        }
+    explicit DSU(int size) : parents_(size), heights_(size) {
+        std::iota(parents_.begin(), parents_.end(), 0);
     }
 
     int find(int index) {
         if (parents_[index] != index) {
             parents_[index] = find(parents_[index]);
         }
-
         return parents_[index];
     }
 
     void unite(int node1, int node2) {
         int root1 = find(node1);
         int root2 = find(node2);
-
         if (root1 == root2) {
             return;
         }
-
-        if (ranks_[root1] < ranks_[root2]) {
+        if (heights_[root1] < heights_[root2]) {
             parents_[root1] = root2;
-        } else if (ranks_[root1] > ranks_[root2]) {
+        } else if (heights_[root1] > heights_[root2]) {
             parents_[root2] = root1;
         } else {
             parents_[root1] = root2;
-
-            ++ranks_[root2]; 
+            ++heights_[root2];
         }
     }
 
   private:
     std::vector<int> parents_;
-    std::vector<int> ranks_;
+    std::vector<int> heights_;
 };
 
 int main() {
-    int n, m;
+    int n;
+    int m;
     std::cin >> n >> m;
 
-    UnionFind union_find(n);
+    DSU union_find(n);
     for (int i = 0; i < m; ++i) {
-        int x, y;
+        int x;
+        int y;
         std::cin >> x >> y;
 
         union_find.unite(x, y);
@@ -57,13 +54,14 @@ int main() {
     std::cin >> q;
 
     for (int i = 0; i < q; ++i) {
-        int x, y;
+        int x;
+        int y;
         std::cin >> x >> y;
 
         if (union_find.find(x) == union_find.find(y)) {
-            std::cout << "YES" << std::endl;
+            std::cout << "YES" << '\n';
         } else {
-            std::cout << "NO" << std::endl;
+            std::cout << "NO" << '\n';
         }
     }
 
