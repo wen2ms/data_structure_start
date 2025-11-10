8b6f3a7228943ac0981d75769a381cb04cd86597
wen2ms <1819509111@qq.com>
2025-11-10 16:07:22 +0800

    Feat: Modular Inverse

--- modular_inverse.cpp
+++ modular_inverse.cpp
@@ -1,18 +1,6 @@
 #include <cstdint>
 #include <iostream>
 
-int64_t fast_pow(int64_t base, int64_t exp) {
-    int64_t result = 1;
-    while (exp > 0) {
-        if ((exp & 1) == 1) {
-            result *= base;
-        }
-        base *= base;
-        exp >>= 1;
-    }
-    return result;
-}
-
 int64_t fast_pow(int64_t base, int64_t exp, int64_t mod) {
     int64_t result = 1;
     base %= mod;
@@ -26,10 +14,14 @@ int64_t fast_pow(int64_t base, int64_t exp, int64_t mod) {
     return result;
 }
 
-int main() {
-    std::cout << fast_pow(3, 3) << '\n';
+// mod is a prime, num is not zero
+int64_t mod_inv(int64_t num, int64_t mod) {
+    return fast_pow(num, mod - 2, mod);
+}
 
-    std::cout << fast_pow(3, 3, 5) << '\n';
+int main() {
+    const int kMod = 5;
+    std::cout << mod_inv(7, kMod) << '\n';
 
     return 0;
 }
