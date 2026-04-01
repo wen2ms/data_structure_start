631e236cb5efb94882ef043ecf978b099d7c713c
wen2ms <1819509111@qq.com>
2026-04-01 18:31:12 +0800

    Chore: Replace "adjacent" with "adjacency"

--- optimal_dijkstra_algorithm.cpp
+++ optimal_dijkstra_algorithm.cpp
@@ -4,8 +4,8 @@
 #include <queue>
 #include <vector>
 
-std::vector<int> dijkstra(const int source, const std::vector<std::vector<std::pair<int, int>>>& adjacent) {
-    int vertex_count = adjacent.size();
+std::vector<int> dijkstra(const int source, const std::vector<std::vector<std::pair<int, int>>>& adjacency) {
+    int vertex_count = adjacency.size();
     std::vector<int> distances(vertex_count, std::numeric_limits<int>::max());
     std::vector<int> prev_nodes(vertex_count, -1);
     distances[source] = 0;
@@ -17,7 +17,7 @@ std::vector<int> dijkstra(const int source, const std::vector<std::vector<std::p
         if (minimum_distance > distances[minimum_node]) {
             continue;
         }
-        for (const auto& [neighbor, weight] : adjacent[minimum_node]) {
+        for (const auto& [neighbor, weight] : adjacency[minimum_node]) {
             if (minimum_distance + weight < distances[neighbor]) {
                 distances[neighbor] = minimum_distance + weight;
                 prev_nodes[neighbor] = minimum_node;
@@ -44,19 +44,19 @@ int main() {
     int vertex_count = 6;
     int edges_count = 10;
     int source = 0;
-    std::vector<std::vector<std::pair<int, int>>> adjacent(vertex_count);
-    adjacent[0].emplace_back(1, 2);
-    adjacent[0].emplace_back(2, 5);
-    adjacent[1].emplace_back(2, 1);
-    adjacent[1].emplace_back(3, 3);
-    adjacent[2].emplace_back(3, 3);
-    adjacent[2].emplace_back(4, 4);
-    adjacent[2].emplace_back(5, 1);
-    adjacent[3].emplace_back(4, 1);
-    adjacent[3].emplace_back(5, 4);
-    adjacent[4].emplace_back(5, 1);
-    std::vector<int> prev_nodes = dijkstra(source, adjacent);
-    // std::vector<int> minimum_distances = dijkstra(source, adjacent);
+    std::vector<std::vector<std::pair<int, int>>> adjacency(vertex_count);
+    adjacency[0].emplace_back(1, 2);
+    adjacency[0].emplace_back(2, 5);
+    adjacency[1].emplace_back(2, 1);
+    adjacency[1].emplace_back(3, 3);
+    adjacency[2].emplace_back(3, 3);
+    adjacency[2].emplace_back(4, 4);
+    adjacency[2].emplace_back(5, 1);
+    adjacency[3].emplace_back(4, 1);
+    adjacency[3].emplace_back(5, 4);
+    adjacency[4].emplace_back(5, 1);
+    std::vector<int> prev_nodes = dijkstra(source, adjacency);
+    // std::vector<int> minimum_distances = dijkstra(source, adjacency);
 
     for (int i = 0; i < vertex_count; ++i) {
         std::vector<int> path = make_path(prev_nodes, i);
--- topological_sort.cpp
+++ topological_sort.cpp
@@ -2,31 +2,28 @@
 #include <queue>
 #include <vector>
 
-std::vector<int> topological_sort(const std::vector<std::vector<int>>& adjacent) {
-    int vertex_count = adjacent.size();
+std::vector<int> topological_sort(const std::vector<std::vector<int>>& adjacency) {
+    int vertex_count = adjacency.size();
     std::vector<int> indegrees(vertex_count);
     std::vector<int> topo_order;
-
     for (int i = 0; i < vertex_count; ++i) {
-        for (const int end : adjacent[i]) {
+        for (const int end : adjacency[i]) {
             ++indegrees[end];
         }
     }
-
     std::queue<int> zero_nodes;
     for (int i = 0; i < vertex_count; ++i) {
         if (indegrees[i] == 0) {
             zero_nodes.push(i);
         }
     }
-
     while (!zero_nodes.empty()) {
         int index = zero_nodes.front();
 
         zero_nodes.pop();
         topo_order.push_back(index);
 
-        for (int neighbor : adjacent[index]) {
+        for (int neighbor : adjacency[index]) {
             --indegrees[neighbor];
 
             if (indegrees[neighbor] == 0) {
@@ -34,27 +31,24 @@ std::vector<int> topological_sort(const std::vector<std::vector<int>>& adjacent)
             }
         }
     }
-
     if (topo_order.size() != vertex_count) {
         return {};
     }
-
     return topo_order;
 }
 
 int main() {
     int vertex_count = 6;
-    std::vector<std::vector<int>> adjacent(vertex_count);
+    std::vector<std::vector<int>> adjacency(vertex_count);
 
-    adjacent[5].push_back(2);
-    adjacent[5].push_back(0);
-    adjacent[4].push_back(0);
-    adjacent[4].push_back(1);
-    adjacent[2].push_back(3);
-    adjacent[3].push_back(1);
+    adjacency[5].push_back(2);
+    adjacency[5].push_back(0);
+    adjacency[4].push_back(0);
+    adjacency[4].push_back(1);
+    adjacency[2].push_back(3);
+    adjacency[3].push_back(1);
 
-    std::vector<int> topo = topological_sort(adjacent);
-
+    std::vector<int> topo = topological_sort(adjacency);
     for (int index : topo) {
         std::cout << index << ' ';
     }
